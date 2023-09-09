3dd92664ec4d26ec3aa45354aba4301eeeaa83e8
NonsoBarn <102404755+NonsoBarn@users.noreply.github.com>
2023-09-09 13:29:43 +0100

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Current Day of the Week
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDayIndex = new Date().getDay();
-  const currentDayElement = document.querySelector(
-    '[data-testid="currentDayOfTheWeek"]'
-  );
-  currentDayElement.textContent = daysOfWeek[currentDayIndex];
+  function updateCurrentDay() {
+    const daysOfWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const currentDayIndex = new Date().getDay();
+    const currentDayElement = document.querySelector(
+      '[data-testid="currentDayOfTheWeek"]'
+    );
+    currentDayElement.textContent = daysOfWeek[currentDayIndex];
+  }
 
   // Current UTC Time
-  const currentUTCTimeElement = document.querySelector(
-    '[data-testid="currentUTCTime"]'
-  );
-  currentUTCTimeElement.textContent = `Current UTC Time: ${Date.now()} milliseconds`;
+  function updateCurrentUTCTime() {
+    const currentUTCTimeElement = document.querySelector(
+      '[data-testid="currentUTCTime"]'
+    );
+    currentUTCTimeElement.textContent = `Current UTC Time: ${Date.now()} milliseconds`;
+  }
+
+  updateCurrentDay();
+  updateCurrentUTCTime();
+  setInterval(updateCurrentDay, 60000); // Update every minute
+  setInterval(updateCurrentUTCTime, 1000); // Update every second
 });
